60a802fe3f8327adc6ffdd9f53e944b0502ac899
Krzysztof Pawełczak <krzychu101096@gmail.com>
2019-10-28 20:40:02 +0100

    Dodawanie pacjentow

--- VetClinic.Data/Data/Clinic/PatientType.cs
+++ VetClinic.Data/Data/Clinic/PatientType.cs
@@ -13,18 +13,28 @@ namespace VetClinic.Data.Data.Clinic
         public int PatientTypeID { get; set; }
 
         [Required(ErrorMessage = "Nazwa jest wymagane")]
+        [Display(Name = "Nazwa")]
         public string Name { get; set; }
+        [Display(Name = "Opis")]
         public string Description { get; set; }
+        [Display(Name = "Czy aktywny")]
         public bool IsActive { get; set; }
+        [Display(Name = "Data dodania")]
         public DateTime AddedDate { get; set; }
+        [Display(Name = "Data modyfikacji")]
         public DateTime? UpdatedDate { get; set; }
 
+        [Display(Name = "Użytkownik dodający")]
         public int? AddedUserID { get; set; } // użytkownik dodający
         [ForeignKey("AddedUserID")]
+
+        [Display(Name = "Użytkownik dodający rodzaj pacjenta")]
         public User PatientTypeAddedUser { get; set; }
 
+        [Display(Name = "Użytkownik modyfikujący")]
         public int? UpdatedUserID { get; set; } // użytkownik modyfikujący
         [ForeignKey("UpdatedUserID")]
+        [Display(Name = "Użytkownik modyfikujący rodzaj pacjenta")]
         public User PatientTypeUpdatedUser { get; set; }
 
         public ICollection<Patient> Patients { get; set; }
---
+++ VetClinic.Intranet/Controllers/PatientTypeController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VetClinic.Data;
+using VetClinic.Data.Data.Clinic;
+
+namespace VetClinic.Intranet.Controllers
+{
+    public class PatientTypeController : Controller
+    {
+        private readonly VetClinicContext _context;
+
+        public PatientTypeController(VetClinicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PatientType
+        public async Task<IActionResult> Index()
+        {
+            var vetClinicContext = _context.PatientTypes.Include(p => p.PatientTypeAddedUser).Include(p => p.PatientTypeUpdatedUser);
+            return View(await vetClinicContext.ToListAsync());
+        }
+
+        // GET: PatientType/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patientType = await _context.PatientTypes
+                .Include(p => p.PatientTypeAddedUser)
+                .Include(p => p.PatientTypeUpdatedUser)
+                .FirstOrDefaultAsync(m => m.PatientTypeID == id);
+            if (patientType == null)
+            {
+                return NotFound();
+            }
+
+            return View(patientType);
+        }
+
+        // GET: PatientType/Create
+        public IActionResult Create()
+        {
+            ViewData["AddedUserID"] = new SelectList(_context.Users, "UserID", "City");
+            ViewData["UpdatedUserID"] = new SelectList(_context.Users, "UserID", "City");
+            return View();
+        }
+
+        // POST: PatientType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PatientTypeID,Name,Description,IsActive,AddedDate,UpdatedDate,AddedUserID,UpdatedUserID")] PatientType patientType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(patientType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddedUserID"] = new SelectList(_context.Users, "UserID", "City", patientType.AddedUserID);
+            ViewData["UpdatedUserID"] = new SelectList(_context.Users, "UserID", "City", patientType.UpdatedUserID);
+            return View(patientType);
+        }
+
+        // GET: PatientType/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patientType = await _context.PatientTypes.FindAsync(id);
+            if (patientType == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddedUserID"] = new SelectList(_context.Users, "UserID", "City", patientType.AddedUserID);
+            ViewData["UpdatedUserID"] = new SelectList(_context.Users, "UserID", "City", patientType.UpdatedUserID);
+            return View(patientType);
+        }
+
+        // POST: PatientType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PatientTypeID,Name,Description,IsActive,AddedDate,UpdatedDate,AddedUserID,UpdatedUserID")] PatientType patientType)
+        {
+            if (id != patientType.PatientTypeID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(patientType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PatientTypeExists(patientType.PatientTypeID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddedUserID"] = new SelectList(_context.Users, "UserID", "City", patientType.AddedUserID);
+            ViewData["UpdatedUserID"] = new SelectList(_context.Users, "UserID", "City", patientType.UpdatedUserID);
+            return View(patientType);
+        }
+
+        // GET: PatientType/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patientType = await _context.PatientTypes
+                .Include(p => p.PatientTypeAddedUser)
+                .Include(p => p.PatientTypeUpdatedUser)
+                .FirstOrDefaultAsync(m => m.PatientTypeID == id);
+            if (patientType == null)
+            {
+                return NotFound();
+            }
+
+            return View(patientType);
+        }
+
+        // POST: PatientType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var patientType = await _context.PatientTypes.FindAsync(id);
+            _context.PatientTypes.Remove(patientType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PatientTypeExists(int id)
+        {
+            return _context.PatientTypes.Any(e => e.PatientTypeID == id);
+        }
+    }
+}
