ce4eaa1225d6497d66db9d25b5a4e741396ed9f3
unknown <wsb2016inf2@outlook.com>
2019-11-02 22:14:24 +0100

    potwierdzenie email przy rejestracji

---
+++ VetClinic.Intranet/Services/SmtpConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace VetClinic.Intranet.Services
+{
+    public class SmtpConfiguration
+    {
+        public string AddressMail { get; set; } // adres mailowy z ktorego wysyłamy maila (vetclinicwsbnl2016@gmail.com)
+        public string Password { get; set; } // jego hasło wsbnlu2016
+        public string MessageText { get; set; } // treść maila
+        public string MessageSubject { get; set; } // temat maila
+        public string MessageTo { get; set; } // adres na jaki wysyłamy maila
+
+        // adres mailowy kliniki: vetclinicwsbnl2016@gmail.com
+        // haslo:wsbnlu2016
+        public SmtpConfiguration()
+        {
+            this.AddressMail = "vetclinicwsbnl2016@gmail.com";
+            this.Password = "wsbnlu2016";
+        }
+        public void send()
+        {
+            using (var message = new MailMessage())
+            {
+                message.To.Add(new MailAddress(MessageTo));
+                message.From = new MailAddress(AddressMail, "VetClinic");
+                message.Subject = MessageSubject;
+                message.Body = MessageText;
+                message.IsBodyHtml = true;
+
+                using (var client = new SmtpClient("smtp.gmail.com"))
+                {
+                    client.Port = 587;
+                    client.EnableSsl = true;
+                    client.UseDefaultCredentials = false;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Credentials = new NetworkCredential(AddressMail, Password);
+                    client.Send(message);
+                }
+            }
+        }
+    }
+}
