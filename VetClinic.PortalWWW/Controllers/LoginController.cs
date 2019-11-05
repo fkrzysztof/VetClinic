4ee8c583fa0e6e311556c2b6215503f323e667eb
jkopka <jackop007@gmail.com>
2019-11-05 23:48:35 +0100

    added register and login

--- VetClinic.PortalWWW/Controllers/LoginController.cs
+++ VetClinic.PortalWWW/Controllers/LoginController.cs
@@ -1,21 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VetClinic.Data;
+using VetClinic.Data.Data.VetClinic;
 
 namespace VetClinic.PortalWWW.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly VetClinicContext _context;
+
+        public LoginController(VetClinicContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Users.ToList());
         }
 
         public IActionResult Registration()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registration(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                ModelState.Clear();
+                ViewBag.Message = user.FirstName + " " + user.LastName + " pomyślnie zarejestrowano konto.";
+            }
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var account = _context.Users.Where(u => u.Login == user.Login && u.Password == user.Password).FirstOrDefault();
+            if (account != null)
+            {
+                HttpContext.Session.SetString("UserID", account.UserID.ToString());
+                HttpContext.Session.SetString("Login", account.Login.ToString());
+                return RedirectToAction("Welcome");
+            }
+            else
+            {
+                ModelState.AddModelError("","Login lub hasło jest niepoprawne.");
+            }
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+           if(HttpContext.Session.GetString("Login") != null)
+           {
+               ViewBag.Login = HttpContext.Session.GetString("Login");
+               return View();
+           }
+           else
+           {
+               return RedirectToAction("Login");
+           }
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
     }
 }
