e0c8e31fa05c203ee2013211a297b0a3103d207c
Krzysztof Pawełczak <krzychu101096@gmail.com>
2019-11-05 20:02:42 +0100

    Aktualnosci jeszcze nie skonczone

---
+++ VetClinic.PortalWWW/Controllers/RecentNewsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VetClinic.Data;
+
+namespace VetClinic.PortalWWW.Controllers
+{
+    public class RecentNewsController : Controller
+    {
+        private readonly VetClinicContext _context;
+
+        public RecentNewsController(VetClinicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RecentNews
+        public  IActionResult Index(int id)
+        {
+
+            ViewBag.ModelRecentNews =
+             (
+                 from recentnews in _context.RecentNews
+                 orderby recentnews.Position
+                 select recentnews
+             ).ToList();
+            var item = _context.RecentNews.Find(id);
+            return View(item);
+        }
+
+    }
+}
