0789dfe6eecfb91b4f951d6a16e7392654c4fe5e
fkrzysztof <krzysztofranczyk@gmail.com>
2019-12-04 23:31:27 +0100

    Harmonogram-Intranet-Portal-PR2

--- VetClinic.PortalWWW/Controllers/ClientPanelController.cs
+++ VetClinic.PortalWWW/Controllers/ClientPanelController.cs
@@ -2,23 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using VetClinic.Data;
+using VetClinic.Data.Helpers;
 
 namespace VetClinic.PortalWWW.Controllers
 {
     public class ClientPanelController : Controller
     {
         private readonly VetClinicContext _context;
+        public DateTime now = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+        ScheduleBlocks sb = new ScheduleBlocks();
 
         public ClientPanelController(VetClinicContext context)
         {
             _context = context;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            ViewBag.UserFromSession = Int32.Parse(HttpContext.Session.GetString("UserID"));
+            while (true)
+            {
+                if (now.DayOfWeek != DayOfWeek.Monday)
+                    now -= new TimeSpan(1, 0, 0, 0, 0);
+                else
+                    break;
+            }
+            sb.First = now;
+            //dodaje kolekcje rezerwacji w zakresie aktualnie ogladanym
+            sb.Reservation = _context.Reservations.Where(w => w.DateOfVisit >= now && w.DateOfVisit <= now.AddDays(7) && w.IsActive == true).ToList();
+            //dodanie kolekcji dni wolnych
+            sb.InaccessibleDay = _context.InaccessibleDays.Select(s => s.Date).ToList();
+            //dodanie kolekcji godzin pracy przychodni
+            sb.ScheduleBlock = _context.ScheduleBlocks.OrderBy(o => o.Time).ToList();
+            ViewBag.UserID = null;
+
+            return View(sb);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind("First,Navigation")] ScheduleBlocks c)
+        {
+            ViewBag.UserFromSession = Int32.Parse(HttpContext.Session.GetString("UserID"));
+            if (c.Navigation == "next")
+                c.First = c.First.Add(new TimeSpan(7, 0, 0, 0, 0));
+            if (c.Navigation == "previous")
+                c.First -= (new TimeSpan(7, 0, 0, 0, 0));
+            sb.First = c.First;
+            sb.Reservation = _context.Reservations.Where(w => w.DateOfVisit >= sb.First && w.DateOfVisit <= sb.First.AddDays(7) && w.IsActive == true).ToList();
+            //dodanie kolekcji dni wolnych
+            sb.InaccessibleDay = _context.InaccessibleDays.Select(s => s.Date).ToList();
+            //dodanie kolekcji godzin pracy przychodni
+            sb.ScheduleBlock = _context.ScheduleBlocks.OrderBy(o => o.Time).ToList();
+
+            return View(sb);
         }
     }
 }
