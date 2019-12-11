bb5898e2374f70a6c984f5d7e35d9d8dc2f9f29a
Tomasz Krzysztof Ggrzegorz <wsb2016inf2@outlook.com>
2019-12-11 18:46:27 +0100

    Tomasz Zarzasdzanie blokami i poprawka w wizytach

--- VetClinic.Data/Data/Clinic/ScheduleBlock.cs
+++ VetClinic.Data/Data/Clinic/ScheduleBlock.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace VetClinic.Data.Data.Clinic
 {
@@ -7,7 +8,14 @@ namespace VetClinic.Data.Data.Clinic
     {
         [Key]
         public int ScheduleBlockID { get; set; }
+        [DataType(DataType.Time, ErrorMessage ="Wprowadź poprawnt czas np 8:30:30")]
+        [Display(Name = "Czas")]
+        [DisplayFormat(DataFormatString = "{0:t}")]
+        [Required(ErrorMessage ="czas jest wymagany")]
         public TimeSpan Time { get; set; }
+        [DataType(DataType.Time, ErrorMessage = "Wprowadź poprawnt czas np 8:30:30")]
+        [Display(Name = "Przedział czasowy")]
+        [DisplayFormat(DataFormatString = "{0:t}")]
         public TimeSpan? TimeInterval { get; set; }
     }
 }
