352784550c9d72e8d6c8dbf0888d306f2aec197f
Tomasz Krzysztof Ggrzegorz <wsb2016inf2@outlook.com>
2019-12-16 21:01:52 +0100

    Czas w liscie blokow oraz podczas usuwania wyswietla sie teraz bez seklund prosze rownierz o sprawdzenie poprawnosci widokow

--- VetClinic.Data/Data/Clinic/ScheduleBlock.cs
+++ VetClinic.Data/Data/Clinic/ScheduleBlock.cs
@@ -8,14 +8,12 @@ namespace VetClinic.Data.Data.Clinic
     {
         [Key]
         public int ScheduleBlockID { get; set; }
-        [DataType(DataType.Time, ErrorMessage ="Wprowadź poprawnt czas np 8:30:30")]
+        [DataType(DataType.Time, ErrorMessage ="Wprowadź poprawnt czas np 8:30")]
         [Display(Name = "Czas")]
-        [DisplayFormat(DataFormatString = "{0:t}")]
         [Required(ErrorMessage ="czas jest wymagany")]
         public TimeSpan Time { get; set; }
-        [DataType(DataType.Time, ErrorMessage = "Wprowadź poprawnt czas np 8:30:30")]
+        [DataType(DataType.Time, ErrorMessage = "Wprowadź poprawnt czas np 8:30")]
         [Display(Name = "Przedział czasowy")]
-        [DisplayFormat(DataFormatString = "{0:t}")]
         public TimeSpan? TimeInterval { get; set; }
     }
 }
