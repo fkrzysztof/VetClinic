d03d417907db9ab35fad6fd25ce7d5acfd8da7f0
fkrzysztof <krzysztofranczyk@gmail.com>
2020-01-09 14:24:26 +0100

    do dalszej pracy

--- VetClinic.Intranet/Controllers/HomeController.cs
+++ VetClinic.Intranet/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VetClinic.Data;
+using VetClinic.Data.Helpers;
 using VetClinic.Intranet.Controllers.Abstract;
 using VetClinic.Intranet.Models;
 using VetClinic.Intranet.Policy;
@@ -22,17 +24,102 @@ namespace VetClinic.Intranet.Controllers
             _logger = logger;
         }
 
+        //public async Task<IActionResult> Index()
+        //{
+        //    //UserPolicy policy = new UserPolicy(_context, HttpContext, this.ControllerContext.RouteData);
+        //    //ViewData = policy.PopulateViewData(ViewData);
+
+        //    if (HttpContext.Session.GetString("Login") != null)
+        //    {
+        //        return View();
+        //    }
+        //    else
+        //        return RedirectToAction("Index", "Login");
+        //}
+
+        // Krzysztof ********************************START*************************************************
+
+        public DateTime now = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+        ScheduleBlocks sb = new ScheduleBlocks();
+
         public async Task<IActionResult> Index()
         {
-            //UserPolicy policy = new UserPolicy(_context, HttpContext, this.ControllerContext.RouteData);
-            //ViewData = policy.PopulateViewData(ViewData);
+            int userid = Convert.ToInt32(HttpContext.Session.GetString("UserID"));
+            int usertypeid = (from item in _context.Users where item.UserID == userid select item.UserTypeID).FirstOrDefault();
+            ViewBag.VetClinicContext = _context.News.Include(n => n.NewsUpdatedUser)
+                                        .Include(n => n.ReceiverUserTypes).Include(n => n.SenderUser)
+                                        .Where(n => n.UserTypeID == usertypeid && n.IsActive == true);
+
+           // return View(await vetClinicContext.OrderByDescending(u => u.UpdatedDate).ToListAsync());
+
+
+            while (true)
+            {
+                if (now.DayOfWeek != DayOfWeek.Monday)
+                    now -= new TimeSpan(1, 0, 0, 0, 0);
+                else
+                    break;
+            }
+            sb.First = now;
+            //dodaje kolekcje rezerwacji w zakresie aktualnie ogladanym
+            sb.Reservation = _context.Reservations.Where(w => w.DateOfVisit >= now && w.DateOfVisit <= now.AddDays(7) && w.IsActive == true).ToList();
+            //dodanie kolekcji dni wolnych
+            sb.InaccessibleDay = _context.InaccessibleDays.Select(s => s.Date).ToList();
+            //dodanie kolekcji godzin pracy przychodni
+            sb.ScheduleBlock = _context.ScheduleBlocks.OrderBy(o => o.Time).ToList();
+            ViewBag.UserID = null;
+            
+            if (HttpContext.Session.GetString("Login") != null)
+            {
+                return View(sb);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind("First,Navigation")] ScheduleBlocks c)
+        {
+            int userid = Convert.ToInt32(HttpContext.Session.GetString("UserID"));
+            int usertypeid = (from item in _context.Users where item.UserID == userid select item.UserTypeID).FirstOrDefault();
+            ViewBag.VetClinicContext = _context.News.Include(n => n.NewsUpdatedUser)
+                                        .Include(n => n.ReceiverUserTypes).Include(n => n.SenderUser)
+                                        .Where(n => n.UserTypeID == usertypeid && n.IsActive == true);
+
+
+            if (c.Navigation == "next")
+                c.First = c.First.Add(new TimeSpan(7, 0, 0, 0, 0));
+            if (c.Navigation == "previous")
+                c.First -= (new TimeSpan(7, 0, 0, 0, 0));
+            sb.First = c.First;
+            sb.Reservation = _context.Reservations.Where(w => w.DateOfVisit >= sb.First && w.DateOfVisit <= sb.First.AddDays(7) && w.IsActive == true).ToList();
+            //dodanie kolekcji dni wolnych
+            sb.InaccessibleDay = _context.InaccessibleDays.Select(s => s.Date).ToList();
+            //dodanie kolekcji godzin pracy przychodni
+            sb.ScheduleBlock = _context.ScheduleBlocks.OrderBy(o => o.Time).ToList();
 
             if (HttpContext.Session.GetString("Login") != null)
             {
-                return View();
-            }else
-            return RedirectToAction("Index", "Login");
+                return View(sb);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
+
+
+
+
+
+
+
+        // Krzysztof *********************************KONIEC***********************************************
+
+
 
         public IActionResult Privacy()
         {
