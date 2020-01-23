d87be282b49157fffe3346d808f48c26a6509c7d
Damian Nowak <me@dnowak.dev>
2020-01-23 12:50:13 +0100

    added new column for reservation

--- VetClinic.Data/Migrations/20200123114430_Add_VisitId_in_Reservation.Designer.cs
+++ VetClinic.Data/Migrations/20200123114430_Add_VisitId_in_Reservation.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VetClinic.Data;
 
 namespace VetClinic.Data.Migrations
 {
     [DbContext(typeof(VetClinicContext))]
-    partial class VetClinicContextModelSnapshot : ModelSnapshot
+    [Migration("20200123114430_Add_VisitId_in_Reservation")]
+    partial class Add_VisitId_in_Reservation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
