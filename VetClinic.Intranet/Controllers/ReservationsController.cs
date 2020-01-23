ec00e1ba97e612d58e33b78131ddfad0e60ed86f
Krzysztof Pawełczak <krzychu101096@gmail.com>
2020-01-23 15:11:38 +0100

    Utworzenie wizyty z rezerwacji intranet  gotowe-testowane

--- VetClinic.Intranet/Controllers/ReservationsController.cs
+++ VetClinic.Intranet/Controllers/ReservationsController.cs
@@ -167,7 +167,7 @@ namespace VetClinic.Intranet.Controllers
                 int UserID = Int32.Parse(HttpContext.Session.GetString("UserID"));
                 var data = DateTime.Today;
                 List<int> listreservationsID = _context.Reservations.Where(v=>v.DateOfVisit>data).Where(v => v.DateOfVisit<data.AddDays(1)).Select(p => p.ReservationID).ToList();
-                var ownReservations = _context.Reservations.Include(p => p.Patients).Include(p=>p.ReservationUser).Where(v => listreservationsID.Contains(v.ReservationID));
+                var ownReservations = _context.Reservations.Include(p => p.Patients).Include(p=>p.ReservationUser).Where(v => listreservationsID.Contains(v.ReservationID)).Where(r=>r.VisitID==null);
 
                 ViewData["CurrentFilter"] = searchString;
                 ViewData["PatientUserID"] = new SelectList(_context.PatientTypes, "PatientUserID", "Name");
@@ -190,6 +190,65 @@ namespace VetClinic.Intranet.Controllers
 
             }
             return View();
+        }
+
+        public IActionResult AddVisitFromReservation(int id)
+        {
+            ViewData["VisitUserID"] = new SelectList(_context.Users, "UserID", "LastName");
+            ViewData["PatientID"] = new SelectList(_context.Patients, "PatientID", "Name");
+            ViewData["VetName"] = new SelectList(_context.Users.Where(u => u.UserTypeID == 2)/*2 to id lekarzy*/, "UserID", "LastName");
+            ViewData["VetID"] = new SelectList(_context.Users.Where(u => u.UserTypeID == 2)/*2 to id lekarzy*/, "UserID", "UserID");
+            ViewData["TreatmentID"] = new SelectList(_context.Treatments, "TreatmentID", "Name");
+            ViewData["AddedUserID"] = new SelectList(_context.Users, "UserID", "LastName");
+            ViewData["VisitUserID"] = new SelectList(_context.Users, "UserID", "LastName");
+            ViewData["MedicineID"] = new SelectList(_context.Medicines, "MedicineID", "Name");
+            ViewData["CzyLekarz"] = _context.Users.Where(a => a.UserID == Int32.Parse(HttpContext.Session.GetString("UserID")))
+                .Select(a => a.UserTypeID).FirstOrDefault() == 2;//typeid dla lekarza
+            ViewData["ZalogowanyLekarz"] = _context.Users.FirstOrDefault(a => a.UserID == Int32.Parse(HttpContext.Session.GetString("UserID"))).Fullname;
+
+            var visit = _context.Reservations.FirstOrDefault(p => p.ReservationID == id);
+            return View(visit);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddVisitFromReservation(int? id, [Bind("VisitID,VisitUserID,PatientID,VetID,TreatmentID,DateOfVisit,Description,IsActive,AddedDate,UpdatedDate,AddedUserID,UpdatedUserID")] Visit visit
+                    , [Bind("PatientID")]Patient patient, Reservation reservation, int id2,int id3)
+        {
+            // ViewData["MedicineID"] = new MultiSelectList(_context.Medicines, "MedicineID", "Name",medicinesID);
+            id = patient.PatientID;
+            id2 = reservation.ReservationID;
+
+
+            //if (ModelState.IsValid)
+            //{
+            visit.VisitUserID = Int32.Parse(
+                _context.Patients.Where(p => p.PatientID == id).Select(a => a.PatientUserID).FirstOrDefault().ToString());
+            visit.PatientID = id;
+            //visit.VetID
+            visit.AddedDate = DateTime.Now;
+            visit.IsActive = true;
+            if (!String.IsNullOrEmpty(HttpContext.Session.GetString("UserID")))
+            {
+                visit.AddedUserID = Int32.Parse(HttpContext.Session.GetString("UserID"));
+            }
+            //do usuniecia jak baza bedize poprawiona
+            visit.TreatmentID = _context.Treatments.Select(p => p.TreatmentID).First();
+
+            _context.Add(visit);
+            await _context.SaveChangesAsync();
+            id3 = visit.VisitID;
+            var res = await _context.Reservations.FindAsync(id2);
+            res.VisitID = id3;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("ShowOwnReservation");
+            //}
+            //ViewData["VisitUserID"] = new SelectList(_context.Users, "UserID", "LastName", visit.VisitUserID);
+            //ViewData["PatientID"] = new SelectList(_context.Patients, "PatientID", "Name", visit.PatientID);
+            //ViewData["VetID"] = new SelectList(_context.Users.Where(u => u.UserTypeID == 2)/*2 to id lekarzy*/, "UserID", "LastName", visit.VetID);
+            //ViewData["TreatmentID"] = new SelectList(_context.Treatments, "TreatmentID", "Name",visit.TreatmentID);
+            //ViewData["AddedUserID"] = new SelectList(_context.Users, "UserID", "LastName", visit.AddedUserID);
+            //ViewData["VisitUserID"] = new SelectList(_context.Users, "UserID", "LastName", visit.UpdatedUserID);
+            //return View(visit);
         }
     }
 }
